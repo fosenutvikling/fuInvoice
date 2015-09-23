33d1a4334fbb82b212b51fb25cf97fef2ff1a968
Jonas Kirkemyr <jonas@fosen-utvikling.no>
2015-09-23 17:10:32 +0200

    require composer files

--- api.php
+++ api.php
@@ -1,4 +1,6 @@
 <?php
+	require __DIR__ . '/vendor/autoload.php';
+
 	$_GET   = filter_input_array(INPUT_GET, FILTER_SANITIZE_STRING);
 	$_POST  = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 
